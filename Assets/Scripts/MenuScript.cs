b0d21795ce56d9c98d36ad391f9f334e1ac3e177
Andrew <andrey.arakelyan@bask.ws>
2020-04-29 05:42:30 +0300

    pr12

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuScript : MonoBehaviour
+{
+
+    GameObject menu;
+    
+    void Start()
+    {
+        menu =  GameObject.Find("Menu");
+    }
+
+    public void StartScene()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Options(GameObject options)
+    {
+        menu.SetActive(!menu.activeSelf);
+        options.SetActive(!options.activeInHierarchy);
+    }
+
+ 
+
+}
